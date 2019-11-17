4b9fa0d5e71ed38eb46a06d2a6e3562cb74fb207
Vishnu Sathyanathan <vishnu@megaexams.com>
2019-11-17 20:34:04 +0530

    Added handler to increment a counter

--- server.go
+++ server.go
@@ -1,22 +1,37 @@
 package main
 
 import (
-    "fmt"
-    "html"
-    "log"
-    "net/http"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
 )
 
+var counter int
+var mutex = &sync.Mutex{}
+
+func echoString(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello , %q", html.EscapeString(r.URL.Path))
+}
+
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(w, strconv.Itoa(counter))
+	mutex.Unlock()
+}
+
 func main() {
+	http.HandleFunc("/", echoString)
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
+	http.HandleFunc("/increment", incrementCounter)
 
-    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hi")
-    })
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
 
-    log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8081", nil))
 
 }
